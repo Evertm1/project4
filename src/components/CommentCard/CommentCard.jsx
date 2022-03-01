ffd16ef209b2419cad86b2b6671e6ec46bfa70bf
Programming <programming@Marcos-MacBook-Pro.local>
2022-02-28 17:15:07 -0800

    initial comment render

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react'
-export default function CommentCard({comments}){
+import { Item, Card } from 'semantic-ui-react'
+export default function CommentCard({comment}){
 
     return (
-        <Segment raised id="comment">
-        <div>Contents of a comment rendered inside of a card</div>
-        {comments?.[0]?.textContent ? comments[0].textContent : null }
-        </Segment>
+        <Card>
+        <Item.Group> 
+       <Item key={comment?._id ? comment._id : null}>
+          
+    
+          <Item.Content>
+            {comment?.textContent ? comment.textContent : null}
+            {comment?.responseTrackName ? comment.responseTrackName : null}
+            
+            <Item.Meta></Item.Meta>
+            <Item.Description>
+            {/* By {project.user.username} */}
+            
+            </Item.Description>
+            {/* <Item.Extra>{project.user.username},</Item.Extra> */}
+          </Item.Content>
+        </Item>
+        </Item.Group>
+        </Card>
+    
     )
 }
--- src/components/CommentFeed/CommentFeed.jsx
+++ src/components/CommentFeed/CommentFeed.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import CommentCard from "../CommentCard/CommentCard";
-export default function CommentFeed({comments}){
+import { Card } from "semantic-ui-react";
+export default function CommentFeed({comments, user, key}){
 
     return (
-        <CommentCard comments={comments}/>
+        <>
+        
+        <h2 >Response Tracks</h2>
+        <Card.Group itemsPerRow= {1} stackable centered >
+          
+        {comments?.map((comment) => {
+          return (
+            <CommentCard
+              comment={comment}
+              key={comment._id}
+              user={user}
+              
+            />
+          );
+        })}
+      </Card.Group>
+      </>
     )
 }
